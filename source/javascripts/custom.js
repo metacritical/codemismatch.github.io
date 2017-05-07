658637cbeafda87b757bcf20131f5f1170570dd5
Neeraj Doharey <neerajlivewire@gmail.com>
2017-05-07 17:43:20 +0530

    Started new slider website

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -1,19 +1,18 @@
-
 // google map
 var map = '';
 var center;
 
 function initialize() {
     var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(23.1131033,72.5982061),
+      zoom: 17,
+      center: new google.maps.LatLng(23.113240, 72.600314),
       scrollwheel: false
     };
   
     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
     var marker = new google.maps.Marker({
-      position: {lat: 23.1131033, lng: 72.5982061},
+      position: {lat: 23.113240, lng:  72.600314},
       map: map
     });
 
@@ -33,86 +32,10 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?key= AIzaSyCrxckNJnzd9Hxxje6ipz_ZXvsYc2EZnRo&' + 'callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDXA_PTUaopZfXDFDXwQgFm_ys20dNad3s&' + 'callback=initialize';
     document.body.appendChild(script);
 }
-
-// Flexslider
 $(function(){
-  /* FlexSlider */
-  $('.flexslider').flexslider({
-      animation: "fade",
-      directionNav: false
-  });
-
-  new WOW().init();
-
   loadGoogleMap();
 });
 
-// isotope
-jQuery(document).ready(function($){
-
-  if ( $('.iso-box-wrapper').length > 0 ) { 
-
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
-
-      $container.imagesLoaded(function () {
-
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
-        });
-
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
-
-      });
-
-      //filter items on button click
-      $('.filter-wrapper li a').click(function(){
-
-          var $this = $(this), filterValue = $this.attr('data-filter');
-
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
-
-      // don't proceed if already selected 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
-
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-        return false;
-      }); 
-
-  }
-
-});
-
-// Hide mobile menu after clicking on a link
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('.navbar-default a, a,').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 68
-            }, 1000);
-            event.preventDefault();
-        });
-    });
